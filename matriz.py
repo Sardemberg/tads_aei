d6f57de40d3cca14ab1cf42c09f91e40641df8dd
Sardemberg <lucassardem@gmail.com>
2022-05-01 16:57:49 -0300

    First commit

---
+++ matriz.py
@@ -0,0 +1,18 @@
+class Matriz:
+    def __init__(self):
+        self.matriz = [[9, 7, 4, 2], [10, 13, 18, 21], [33, 5, 7, 90], [23, 31, 51, 60]]
+
+    def mostrar_matriz(self):
+        # Mostrar matriz original
+
+    def mostrar_transposta(self):
+        # Mostrar matriz transposta
+
+    def soma_diagonal_princial(self):
+        # Soma diagonal principal
+
+    def numeros_pares(self):
+        # Numeros pares
+
+    def numeros_impares(self):
+        # Numeros ímpares
